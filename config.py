f83bde70bd722a8b1fe5c61de2a97fd92b6b8621
ckranthi <ckranthi5555@gmail.com>
2024-04-07 16:52:44 +0530

    intial commit

---
+++ config.py
@@ -0,0 +1,20 @@
+DB_DETAILS = {
+    'dev': {
+        'SOURCE_DB': {
+            'DB_TYPE': 'mysql',
+            'DB_HOST': '54.206.88.157',
+            'DB_NAME': 'mydatabase',
+            'DB_USER': 'retail_db',
+            'DB_PASSWORD': 'itversity'
+
+        },
+        'TARGET_DB': {
+            'DB_TYPE': 'postgres',
+            'DB_HOST': '54.206.88.157',
+            'DB_NAME': 'mydatabase',
+            'DB_USER': 'retail_db',
+            'DB_PASSWORD': 'itversity'
+    }
+
+}
+}
